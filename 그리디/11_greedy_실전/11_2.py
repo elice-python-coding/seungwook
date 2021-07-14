0f0b89fd191316870956d41e596ef6a4a43bea0d
BSW <52040901+tbvjqkdlcm1@users.noreply.github.com>
2021-07-15 00:02:08 +0900

    Update 11_2.py

--- 그리디/11_greedy_실전/11_2.py
+++ 그리디/11_greedy_실전/11_2.py
@@ -4,12 +4,10 @@ while len(my_list) > 1:
     mul = int(my_list[0]) * int(my_list[1])
     sum = int(my_list[0]) + int(my_list[1])
     if mul >= sum:
-        del my_list[0]
-        del my_list[0]
+        del my_list[0:2]
         my_list.insert(0, mul)
     else:
-        del my_list[0]
-        del my_list[0]
+        del my_list[0:2]
         my_list.insert(0, sum)
 
 print(my_list[0])
